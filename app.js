a46f4ae0192601bf086aa22686f170a5bd20daf0
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-12 01:07:41 +0530

    Learned about parsing incoming request and limiting middle and used body parser

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express=require('express');
 const app=express();
+const bodyParser=require('body-parser');
 
-app.use('/app-product',(req, res, next)=>{
-    console.log("In the another Middleware");
-    res.send('<h1>The add product</h1>');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/add-product',(req, res, next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Set Tittle</button></form>');
+})
+app.post('/product',(req, res, next)=>{
+    const title=req.body;
+    console.log(title.title);
+    res.redirect('/');
 })
 app.use('/',(req, res, next)=>{
-    console.log("In this  Middleware");
-    res.send('<h1>Hello</h1>');
+    res.send('<h1>Hello Express</h1>');
 })
 
 
