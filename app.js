1bf2bb6d0d7cf669bb327f43f28155cf12c93a73
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-11 15:53:23 +0530

    Learned Express Js Basics and Routes handling

--- app.js
+++ app.js
@@ -1,38 +1,20 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
+const express=require('express');
+const app=express();
 
-    const url=req.url;
-    const method=req.method;
-    if(url==='/'){
-        res.write('<html><head><title></title></head><body><h1>Welcome to the website</h1>');
-        res.write('<h1>UserName</h1>');
-        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Create</button></form>');
-        res.write('</body></html>');
-        res.end();
-    }
-    if(url==='/user'){
-        res.write('<html><head><title></title></head><body><h1>User1</h1>');
-        res.write('<h1>User2</h1>');
-        res.write('<h1>User3</h1>');
-        res.write('</body></html>');
-        res.end();
-    }
-    if(url=== '/create-user'&& method==='POST'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-        })
-        req.on('end',()=>{
-            const parsedBody=Buffer.concat(body).toString();
-            const username=(parsedBody).split('=')[1];
-            console.log(username);
-        })
+app.use('/app-product',(req, res, next)=>{
+    console.log("In the another Middleware");
+    res.send('<h1>The add product</h1>');
+})
+app.use('/',(req, res, next)=>{
+    console.log("In this  Middleware");
+    res.send('<h1>Hello</h1>');
+})
 
-        res.statusCode=302;//302 Means Redirecting
-        res.setHeader('Location','/');
-        return res.end();
-    }
 
-});
-server.listen(3000);
+
+
+
+
+
+app.listen(3000);
 
