a5036709699fa928742a94a4da0a18e5aa56d3f3
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-12 13:44:43 +0530

    Filtering path using Express Router

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const shopRoutes=require('./routes/shop')
 app.use(bodyParser.urlencoded({extended:false}));//Body Parser
 
 
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use('/',(req,res,next)=> {
     res.status(404).send('<h1>Page not Found</h1>')
