cefe2f16fda715c1a52f9ffe61aa5dd2cf07318d
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-12 14:37:19 +0530

    Used A Helper function to get root directory in a variable to make code look simpler

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser');
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 const path=require('path');
-
+const rootDir=require('./util/path');
 
 app.use(bodyParser.urlencoded({extended:false}));//Body Parser
 
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended:false}));//Body Parser
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use('/',(req,res,next)=> {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 });
 
 app.listen(3000);
