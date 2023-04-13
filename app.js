b25fd09aee3d119f9ece8cb671457c10cab7f9c4
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-13 15:32:04 +0530

    Added Layouts and Completed Using Pugs

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(express.static(path.join(rootDir,'public')));
 app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 app.use('/',(req,res,next)=> {
-    res.status(404).render('404')
+    res.status(404).render('404',{pageTitle:'404'})
 });
 
-app.listen(3000);
+app.listen(8080);
 
 //Section 5 Completed
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const rootDir=require('../util/path');
 
 const products=[];
 router.get('/add-product',(req, res, next)=>{
-    res.render('add-product',{pageTitle:"Add"});
+    res.render('add-product',{pageTitle:"Add-Product",path: '/admin/add-product'});
 })
 router.post('/product',(req, res, next)=>{
     products.push({title : req.body.title});
