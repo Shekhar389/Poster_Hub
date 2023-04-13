09723a0ecac159674c644a9573fd582f71e9bca1
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-13 14:27:16 +0530

    Converted all html files into pug

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const adminData=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
 const path=require('path');
 const rootDir=require('./util/path');
+app.set('view engine','pug');
+app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));//Body Parser
 app.use(express.static(path.join(rootDir,'public')));
@@ -12,7 +14,7 @@ app.use(express.static(path.join(rootDir,'public')));
 app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 app.use('/',(req,res,next)=> {
-    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
+    res.status(404).render('404')
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -6,11 +6,10 @@ const rootDir=require('../util/path');
 
 const products=[];
 router.get('/add-product',(req, res, next)=>{
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
+    res.render('add-product',{pageTitle:"Add"});
 })
 router.post('/product',(req, res, next)=>{
     products.push({title : req.body.title});
-    console.log({title : req.body.title});
     res.redirect('/');
 })
 exports.routes=router;
