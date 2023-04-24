0d6406d1a7180195057079e547050ee8ca0986b6
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-24 20:00:03 +0530

    Delete product added and start working on cart view

--- models/cart.js
+++ models/cart.js
@@ -34,10 +34,17 @@ module.exports=class Cart{
         fs.readFile(p,(err,fileContent)=>{
             if(err)
             {
-               return 
+               return; 
             }
-            const 
-            
+            const updatedCart={...JSON.parse(fileContent)};
+            const product=updatedCart.products.find(prod=> prod.id===id)  
+            const productQty=product.qty;
+            updatedCart.products=updatedCart.products.filter(
+                prod=> prod.id!==id
+            );
+            updatedCart.totalPrice=updatedCart.totalPrice-productPrice*productQty;   
+            fs.writeFile(p,JSON.stringify(updatedCart),err =>{
+                console.log(err);});       
         })
 
     }
