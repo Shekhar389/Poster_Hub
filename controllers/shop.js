046bec63169e40a8b60ef93ca24d23b97ca29a6c
Shekhar Kumar <kshekhar2807@gmail.com>
2023-04-26 20:43:36 +0530

    Successfully Completed Cart Section with all functionalities

--- controllers/shop.js
+++ controllers/shop.js
@@ -34,21 +34,26 @@ exports.getProducts=(req, res, next)=>{
     });
    };
 
-   exports.getCart=(req,res,next)=>{
-    Cart.getCart(cart=>{
-
-        Product.fetchAll(products=>{
-            for(product of products){
-                if(cart.prod)
-            }
-        })
-        res.render('shop/cart',{
-            path :'/cart',
-            pageTitle: 'Your Cart',
+   exports.getCart = (req, res, next) => {
+    Cart.getCart(cart => {
+      Product.fetchAll(products => {
+        const cartProducts = [];
+        for (product of products) {
+          const cartProductData = cart.products.find(
+            prod => prod.id === product.id
+          );
+          if (cartProductData) {
+            cartProducts.push({ productData: product, qty: cartProductData.qty });
+          }
+        }
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: cartProducts
         });
-    })
-    
-   }
+      });
+    });
+  };
    exports.postCart=(req,res,next)=>{
         const prodId=req.body.productId;
         Product.findById(prodId,(product)=>{
@@ -56,6 +61,13 @@ exports.getProducts=(req, res, next)=>{
         })
         res.redirect('/cart');
    };
+   exports.postCartDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, product => {
+      Cart.deleteProduct(prodId, product.price);
+      res.redirect('/cart');
+    });
+  };
    exports.getOrders=(req,res,next)=>{
     res.render('shop/orders',{
         path :'/orders',
